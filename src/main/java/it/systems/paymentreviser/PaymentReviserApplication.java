762f760953bcdd081819d2a8084eade7ef351a8d
lukas-petraitis <lukas.petraitis@zenitech.co.uk>
2024-02-11 13:57:51 +0200

    initial commit

---
+++ src/main/java/it/systems/paymentreviser/PaymentReviserApplication.java
@@ -0,0 +1,13 @@
+package it.systems.paymentreviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentReviserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/systems/paymentreviser/PaymentReviserApplicationTests.java
@@ -0,0 +1,13 @@
+package it.systems.paymentreviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
